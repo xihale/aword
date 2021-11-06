d63190c395ea655fc4c7551b1d38f2d7704e217a
xihale <xihale.top@qq.com>
2021-11-06 18:46:12 +0800

    Update by xihale

---
+++ index.js
@@ -0,0 +1,12 @@
+var get=()=>fetch("https://v1.hitokoto.cn/?c=i").then(res=>res.json()).then(data=>{
+    document.querySelector('#main').innerHTML=data.hitokoto;
+    console.log(data.hitokoto);
+});
+get();
+document.onkeydown=function(e){
+    e = e || window.event;
+    if ((e.ctrlKey && e.key=='r') || e.key == 'F5') { 
+        get();
+        return false;
+    }
+};
