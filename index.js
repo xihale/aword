3691d56ebf3902e92a55050506e426e54c03a8ea
xihale <xihale.top@qq.com>
2021-11-13 20:14:03 +0800

    修改了诺干bug

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+var get=()=>fetch("https://v1.hitokoto.cn/?c=i").then(res=>res.json()).then(data=>{
+    document.querySelector('#main').innerHTML=data.hitokoto;
+    console.log(data.hitokoto);
+});
+document.onkeydown=function(e){
+    e = e || window.event;
+    if ((e.ctrlKey && e.key=='r') || e.key == 'F5') { 
+        get();
+        return false;
+    }
+};
 if(location.hash!=''){
     document.querySelector('#main').innerHTML=decodeURI(location.hash.substring(1));
 }else{
-    var get=()=>fetch("https://v1.hitokoto.cn/?c=i").then(res=>res.json()).then(data=>{
-        document.querySelector('#main').innerHTML=data.hitokoto;
-        console.log(data.hitokoto);
-    });
     get();
-    document.onkeydown=function(e){
-        e = e || window.event;
-        if ((e.ctrlKey && e.key=='r') || e.key == 'F5') { 
-            get();
-            return false;
-        }
-    };
 }
